a9c71dc21265f0f4a87caac293cefcc8714befae
Daria Sokolova <dasha-inet@mail.ru>
2019-11-14 22:21:38 +0400

    feat: initial

---
+++ 4x4source.js
@@ -0,0 +1,7 @@
+window.image4src = 
+[
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["FFEB3B", "FFC107","FFC107","FFEB3B"],
+    ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
+]
